6422dc6e9d0072c9fd47a0b7df12d4bd5467dec7
Jomar Rabanera <jgrabanera@gmail.com>
2026-09-07 01:00:38 +0800

    user

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -12,7 +12,7 @@ class AccountSeeder extends Seeder
         $rows = [
             0 => [
                 'id' => 1,
-                'name' => 'Jomar Rabanera',
+                'name' => 'SB4G Admin',
                 'email' => 'admin.user@sb4g.com',
                 'email_verified_at' => '2026-09-05 07:32:34',
                 'password' => '$2y$12$IgyviFQySA11CmA1ktWtMeJk.MQJyDN.bXl4SWLJNvJt6PJ.71DzK',
